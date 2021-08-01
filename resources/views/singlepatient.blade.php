6525469c41039caf52a0dd2242366db869415222
Kelechi Okwuriki <kelechi@bento.africa>
2021-08-01 18:49:31 +0100

    Styling

--- resources/views/singlepatient.blade.php
+++ resources/views/singlepatient.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     @if (session('status'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
             {{ session('status') }}
 
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
